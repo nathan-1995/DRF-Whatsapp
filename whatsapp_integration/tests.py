04e9eabb02138ea0426daa50f5e73d93f716e806
Nathan <ndelima1@gmail.com>
2025-01-23 20:00:49 +0530

    added test case

--- whatsapp_integration/tests.py
+++ whatsapp_integration/tests.py
@@ -1,3 +1,24 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+class SendWhatsAppMessageTest(APITestCase):
+    def test_send_whatsapp_message(self):
+        url = '/api/send-message/'  # URL of the API endpoint
+
+        # Data to be sent in the request
+        data = {
+            "sender": "+14155238886",
+            "receiver": "+######",  # Replace with a valid WhatsApp number
+            "content": "Running TEST.PY."
+        }
+
+        # Make the request
+        response = self.client.post(url, data, format='json')
+
+        # Log the response to debug
+        print("Response status code:", response.status_code)
+        print("Response data:", response.data)
+
+        # Check if the response is successful
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data.get('message'), 'Message sent successfully.')
+        
